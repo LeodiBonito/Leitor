646250125869b3d21c3a3b377cceec6e595d22df
Joao Guilherme <jguilherme070105@gmail.com>
2025-05-17 14:00:48 -0300

    telas eventosInscritos e meusEventos; Botões de voltar; :D

---
+++ app/src/main/java/com/example/leitor/eventosInscritos.java
@@ -0,0 +1,27 @@
+package com.example.leitor;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class eventosInscritos extends AppCompatActivity {
+    private Button btnVoltar;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_eventos_inscritos);
+
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+
+        btnVoltar.setOnClickListener(v -> finish());
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/leitor/meusEventos.java
@@ -0,0 +1,25 @@
+package com.example.leitor;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class meusEventos extends AppCompatActivity {
+    private Button btnVoltar;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_meus_eventos);
+
+
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+
+        btnVoltar.setOnClickListener(v -> finish());
+    }
+}
